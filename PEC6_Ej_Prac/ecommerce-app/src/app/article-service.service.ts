8236d03b83f9e1b39024380352b38aff9093c44b
jramma <pepe.ramos@mortensen.cat>
2024-05-30 19:15:38 +0200

    ng generate service article-service

---
+++ PEC6_Ej_Prac/ecommerce-app/src/app/article-service.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+
+export class ArticleServiceService {
+
+  constructor() { }
+}
+// ng generate service article-service
