9580f6b68daebd8268a3757f40a97314261fab31
jramma <pepe.ramos@mortensen.cat>
2024-06-07 12:58:44 +0200

    fetch is important for APIs

--- PEC6_Ej_Prac/ecommerce/src/app/app.config.server.ts
+++ PEC6_Ej_Prac/ecommerce/src/app/app.config.server.ts
@@ -1,11 +1,22 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
+import {
+  mergeApplicationConfig,
+  ApplicationConfig,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withFetch,
+} from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering(), importProvidersFrom(HttpClientModule)],
+  providers: [
+    provideServerRendering(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(HttpClientModule),
+  ],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
