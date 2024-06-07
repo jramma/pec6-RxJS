6157130d8623342811501f37165d52aa7e69f072
jramma <pepe.ramos@mortensen.cat>
2024-06-07 12:45:51 +0200

    🐛 some bugs

--- PEC6_Ej_Prac/ecommerce/src/app/app.config.server.ts
+++ PEC6_Ej_Prac/ecommerce/src/app/app.config.server.ts
@@ -1,9 +1,11 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { importProvidersFrom } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
+  providers: [provideServerRendering(), importProvidersFrom(HttpClientModule)],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
